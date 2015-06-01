47e4e3476c9bf6f3e62bf2633ed0657cf6e2edbb
Chris Grovers <chrisgrovers@gmail.com>
2015-06-01 16:15:31 -0700

    [feat] adds routing algorithm. Added function getStart which takes a callback so that api request for start latLong can now be used later. If not given an end destination, will create a route

--- server/server.js
+++ server/server.js
@@ -38,18 +38,61 @@ app.get('/api/result', function(req, res) {
  
 // get geocode latlong data
 app.get('/api/route', function(req, res) {
-  var address = req.body.startLocation.address || '611 Mission St, San Francisco, CA 94105';
-  address = address.replace(' ', '+');
+  var startAddress = req.body.startLocation || '611 Mission St, San Francisco, CA 94105';
+  startAddress = startAddress.replace(' ', '+');
+  // check if there is an end address. If no end address, then need to make a route
+  if (req.body.destinationLocation) {
+    var destAddress = req.body.destinationLocation || null;
+    destAddress = destAddress.replace(' ', '+');
+  }
+  var distance = req.body.distance || 3;
   var coordinates = {};
-  var url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=';
-  request(url, function(error, response, body) {
-    if (!error && res.statusCode === 200) {
-      coordinates = JSON.parse(body).results[0].geometry.location;
-      res.json(coordinates);
-    } else {
-      console.error(error);
-    }
-  });
+  var start = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + startAddress + '&key=';
+  var end = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + destAddress + '&key=';
+  var getStart = function(callback) {
+    return request(start, function(error, response, body) {
+      if (!error && res.statusCode === 200) {
+        console.log('line 55 coordinates start are', JSON.parse(body).results[0].geometry.location);
+        callback(JSON.parse(body).results[0].geometry.location);
+      } else {
+        console.error(error);
+      }
+    })
+  }
+  getStart(function(startPoint) {
+      coordinates.start = startPoint;
+      console.log('line 63 the startpoint is ', coordinates.start);
+      if (destAddress) {
+        request(end, function(error, response, body) {
+          if (!error && res.statusCode === 200) {
+            coordinates.end = JSON.parse(body).results[0].geometry.location;
+            res.json(coordinates);
+          } else {
+            console.error(error);
+          }
+        });
+      } else {
+        coordinates.wayPoints = [];
+        // 0.01 in longitude is 1.2 miles in san francisco
+        var longConvert = function(longitude) { // converts distance up into how much to add to longitude
+          return longitude * 0.00833333333;
+        }
+        // 0.01 in lattitude is 0.6 miles in san francisco
+        var latConvert = function(lattitude) {
+          return lattitude * 0.01666666666;
+        }
+        // distance is 3
+        // need to determine a distUp, and distDown that will route back to start with a distance of about 3
+        // create a route in a triangle shape with each side whose ratios are 3:4:5
+        var trianglePerim = distance/12; 
+        var distUp = longConvert(3 * trianglePerim);
+        var distLeft = latConvert(4 * trianglePerim);
+        coordinates.wayPoints.push({'lat':coordinates.start.lat + distUp, 'lng':coordinates.start.lng});
+        coordinates.wayPoints.push({'lat':coordinates.start.lat, 'lng':coordinates.start.lng + distLeft});
+        console.log('waypoints are', coordinates.wayPoints);
+        res.json(coordinates);
+      }
+    })
 });
 
 app.listen(port);
