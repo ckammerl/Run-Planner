5478214034169cdab5ca01f05df620df3d4d348b
Christiane Kammerl <christiane@kammerl.com>
2015-06-01 17:29:44 -0700

    [feat] passes user entry data from /search (input) to /result view - 3 nested (child) views also have access to passed params via $stateParams

--- client/views/result/result.weather.js
+++ client/views/result/result.weather.js
@@ -1,16 +1,9 @@
 angular.module('runPlannerApp')
 
   .controller('WeatherController', function($scope, $stateParams) {
-    /** final data
-    $scope.celsius = $stateParams.temp.C;
-    $scope.fahrenheit = $stateParams.temp.F;
+    //final version
+    $scope.celsius = $stateParams.celsius;
+    $scope.fahrenheit = $stateParams.fahrenheit;
     $scope.humidity = $stateParams.humidity;
     $scope.wind = $stateParams.wind;
-  */
-
-    // test data
-    $scope.celsius = 20;
-    $scope.fahrenheit = 60;
-    $scope.humidity = 20;
-    $scope.wind = 50.9;
   });
--- client/views/search/search.js
+++ client/views/search/search.js
@@ -1,10 +1,18 @@
-angular.module("runPlannerApp")
+angular.module('runPlannerApp')
 
-.controller("SearchController", function($scope, Search, $state) {
+.controller('SearchController', function($scope, $state, Search) {
+
   $scope.sendInput = function() {
+    /** final data
     Search.sendInput($scope.search)
-      .then(function(resultFromAPI) {
-        $state.go("result", resultFromAPI);
-      })
-  }
+       .then(function(resultFromAPI) {
+         $state.go('result', resultFromAPI);
+       })
+  */
+
+    // test data for weather
+    // go to /result; params accessible to all child views
+    $state.go('result', { celsius: 25, fahrenheit: 80, humidity: 55, wind: 73.5 });
+  };
 });
+
