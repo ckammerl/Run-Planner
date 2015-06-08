ba3abfd3bc4a2b566702d4c6c4136d840380e776
Nathan Lin <nlin.wei.an@gmail.com>
2015-06-08 11:28:33 -0700

    [feat] added form validation on the search page

--- client/views/search/search.js
+++ client/views/search/search.js
@@ -3,22 +3,38 @@ angular.module('runPlannerApp')
 .controller('SearchController', function($scope, $state, Search, ResultHandler) {
 
   $scope.useSearchToGetResult = function() {
-    Search.getZipCode($scope.search)
-      .then(function(zipCode) {
-        Search.getWeather(zipCode)
+    if ($scope.search) {
+      if (!$scope.search.start) {
+        $scope.addressError = true;
+      }
+      if (!$scope.search.distance) {
+        $scope.distanceError = true;
+      }
+      if (!$scope.search.gender) {
+        $scope.genderError = true;
+      }
+      if ($scope.addressError && $scope.distanceError && $scope.genderError) {
+        Search.getZipCode($scope.search)
+        .then(function(zipCode) {
+          Search.getWeather(zipCode)
           .then(function(weather) {
             ResultHandler.setWeather(weather);
             Search.getClothing($scope.search.gender, weather)
-              .then(function(clothing) {
-                ResultHandler.setClothing(clothing);
-                $state.go('result');
-              })
+            .then(function(clothing) {
+              ResultHandler.setClothing(clothing);
+              $state.go('result');
+            })
           })
-      })
-    Search.getRoute($scope.search)
-      .then(function(route) {
-        ResultHandler.setRoute(route);
-      })
+        })
+        Search.getRoute($scope.search)
+        .then(function(route) {
+          ResultHandler.setRoute(route);
+        })
+      }
+    } else {
+      $scope.addressError = true;
+      $scope.distanceError = true;
+      $scope.genderError = true;
+    }
   }
-
 });
