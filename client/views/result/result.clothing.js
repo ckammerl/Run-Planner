b3d35eb17d7f3041c3a6f8f41bc4d8477e965186
Christiane Kammerl <christiane@kammerl.com>
2015-06-08 17:43:58 -0700

    [fix] solves merge conflict

--- client/views/result/result.clothing.js
+++ client/views/result/result.clothing.js
@@ -1,7 +1,7 @@
 angular.module('runPlannerApp')
 
 .controller('ClothingController', function($scope, ResultHandler) {
-  // currently no gender info available, only uni top and bottom
+  // displayed top and bottom clothing is gender specific
   $scope.topCloth = ResultHandler.clothing.top || 'shirt';
   $scope.bottomCloth = ResultHandler.clothing.bottom || 'shorts';
 });
